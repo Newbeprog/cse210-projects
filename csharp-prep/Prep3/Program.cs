f030e8d9b5fac6bde80f79c0217e903359167887
Newbeprog <113536782+Newbeprog@users.noreply.github.com>
2023-09-23 19:49:54 -0600

    Prep 3 Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,46 @@
 using System;
-
+using System.Security.Cryptography;
+//Programing with Class
+//Prep 3 
+//Kamas Peterson
+//Get the user to guess the magic number
+//(c) 2023
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random numberGenorator = new Random();
+        int magicNumber = numberGenorator.Next(1,100);
+
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            int kpyourguess = int.Parse(Console.ReadLine());
+
+
+            if (kpyourguess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            
+            else if (kpyourguess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        
+        
+        
+        
     }
 }
