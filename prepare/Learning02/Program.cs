f4c08e91be86d93d3837c6ef8f9657d57e4fe332
Newbeprog <113536782+Newbeprog@users.noreply.github.com>
2023-09-25 11:27:30 -0600

    finished learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Computer Science"; 
+        job1._company = "Ancestory"; 
+        job1._startyear = 2027;
+        job1._endyear = 2031;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Microsoft";
+        job2._startyear = 2031;
+        job2._endyear = 2036;
+
+        Resume myResume = new Resume();
+        myResume._name = "Kamas Cecily";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
